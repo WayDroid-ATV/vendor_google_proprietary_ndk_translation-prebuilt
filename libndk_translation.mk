58d9ea08c4c882db962d3932ceee980739017c81
SupeChicken666 <me@supechicken666.dev>
2026-02-25 02:51:58 +0800

    Pull ndk_translation_arm64 from AVD15
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- libndk_translation.mk
+++ libndk_translation.mk
@@ -2,16 +2,15 @@
 WITH_NATIVE_BRIDGE := true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.dalvik.vm.isa.arm=x86 \
-    ro.vendor.enable.native.bridge.exec=1 \
-    ro.dalvik.vm.isa.arm64=x86_64 \
-    ro.vendor.enable.native.bridge.exec64=1 \
+	ro.dalvik.vm.isa.arm=x86 \
+	ro.vendor.enable.native.bridge.exec=1 \
+	ro.dalvik.vm.isa.arm64=x86_64 \
+	ro.vendor.enable.native.bridge.exec64=1 \
 	ro.dalvik.vm.native.bridge=libndk_translation.so \
 	ro.ndk_translation.version=0.2.3 \
 	ro.ndk_translation.flags=accurate-sigsegv
 
 PRODUCT_PACKAGES += \
-	ndk_translation_program_runner_binfmt_misc \
 	ndk_translation_program_runner_binfmt_misc_arm64 \
 	ndk_translation.rc
 
