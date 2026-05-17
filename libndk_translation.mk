e7d76d400f87b0faa544387671930f88bf6ae5f0
SupeChicken666 <me@supechicken666.dev>
2026-05-17 20:50:30 +0800

    Build proxy libraries from AOSP
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- libndk_translation.mk
+++ libndk_translation.mk
@@ -1,5 +1,5 @@
 # Bundle libndk_translation as ARM on x86 native bridge
-WITH_NATIVE_BRIDGE := true
+include frameworks/libs/native_bridge_support/native_bridge_support.mk
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.berberis.flags=accurate-sigsegv \
@@ -7,7 +7,37 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.dalvik.vm.native.bridge=libndk_translation.so \
 	ro.dalvik.vm.isa.arm=x86 \
 	ro.dalvik.vm.isa.arm64=x86_64 \
-	ro.enable.native.bridge.exec=1 \
-	ro.ndk_translation.version=16.0.0
+	ro.enable.native.bridge.exec=1
 
-PRODUCT_PACKAGES += ndk_translation_program_runner
+PRODUCT_PACKAGES += \
+	$(NATIVE_BRIDGE_PRODUCT_PACKAGES) \
+	libberberis_exec_region \
+	libndk_translation_proxy_libEGL \
+	libndk_translation_proxy_libGLESv1_CM \
+	libndk_translation_proxy_libGLESv2 \
+	libndk_translation_proxy_libGLESv3 \
+	libndk_translation_proxy_libOpenMAXAL \
+	libndk_translation_proxy_libOpenSLES \
+	libndk_translation_proxy_libaaudio \
+	libndk_translation_proxy_libamidi \
+	libndk_translation_proxy_libandroid \
+	libndk_translation_proxy_libandroid_runtime \
+	libndk_translation_proxy_libbinder_ndk \
+	libndk_translation_proxy_libc \
+	libndk_translation_proxy_libcamera2ndk \
+	libndk_translation_proxy_libjnigraphics \
+	libndk_translation_proxy_libmediandk \
+	libndk_translation_proxy_libnativehelper \
+	libndk_translation_proxy_libnativewindow \
+	libndk_translation_proxy_libneuralnetworks \
+	libndk_translation_proxy_libvulkan \
+	libndk_translation_proxy_libwebviewchromium_plat_support \
+	libndk_translation \
+	ndk_translation_program_runner_binfmt_misc \
+	ndk_translation.rc \
+	cpuinfo.arm.txt \
+	cpuinfo.arm64.txt \
+	ld.config.arm.txt \
+	ld.config.arm64.txt
+
+PRODUCT_SOONG_NAMESPACES += frameworks/libs/native_bridge_support/android_api/libc
