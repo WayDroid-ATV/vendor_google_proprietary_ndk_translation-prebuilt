1236564dcb7e907ed8dc4d5c8fa7ce6dd0e14630
SupeChicken666 <me@supechicken666.dev>
2026-08-23 12:53:45 +0800

    Pull proxy implementations from AVD17 QPR0
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- libndk_translation.mk
+++ libndk_translation.mk
@@ -10,32 +10,32 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGES += \
 	$(NATIVE_BRIDGE_PRODUCT_PACKAGES) \
+    berberis_cpuinfo_arm64 \
+    ld.config.arm64.txt \
 	libberberis_exec_region \
-	libndk_translation_proxy_libEGL \
-	libndk_translation_proxy_libGLESv1_CM \
-	libndk_translation_proxy_libGLESv2 \
-	libndk_translation_proxy_libGLESv3 \
-	libndk_translation_proxy_libOpenMAXAL \
-	libndk_translation_proxy_libOpenSLES \
-	libndk_translation_proxy_libaaudio \
-	libndk_translation_proxy_libamidi \
-	libndk_translation_proxy_libandroid \
-	libndk_translation_proxy_libandroid_runtime \
-	libndk_translation_proxy_libbinder_ndk \
-	libndk_translation_proxy_libc \
-	libndk_translation_proxy_libcamera2ndk \
-	libndk_translation_proxy_libjnigraphics \
-	libndk_translation_proxy_libm \
-	libndk_translation_proxy_libmediandk \
-	libndk_translation_proxy_libnativehelper \
-	libndk_translation_proxy_libnativewindow \
-	libndk_translation_proxy_libneuralnetworks \
-	libndk_translation_proxy_libvulkan \
-	libndk_translation_proxy_libwebviewchromium_plat_support \
 	libndk_translation \
+    libproxy_arm64_to_x86_64_libEGL \
+    libproxy_arm64_to_x86_64_libGLESv1_CM \
+    libproxy_arm64_to_x86_64_libGLESv2 \
+    libproxy_arm64_to_x86_64_libGLESv3 \
+    libproxy_arm64_to_x86_64_libOpenMAXAL \
+    libproxy_arm64_to_x86_64_libOpenSLES \
+    libproxy_arm64_to_x86_64_libaaudio \
+    libproxy_arm64_to_x86_64_libamidi \
+    libproxy_arm64_to_x86_64_libandroid \
+    libproxy_arm64_to_x86_64_libandroid_runtime \
+    libproxy_arm64_to_x86_64_libbinder_ndk \
+    libproxy_arm64_to_x86_64_libc \
+    libproxy_arm64_to_x86_64_libcamera2ndk \
+    libproxy_arm64_to_x86_64_libjnigraphics \
+    libproxy_arm64_to_x86_64_libm \
+    libproxy_arm64_to_x86_64_libmediandk \
+    libproxy_arm64_to_x86_64_libnativehelper \
+    libproxy_arm64_to_x86_64_libnativewindow \
+    libproxy_arm64_to_x86_64_libneuralnetworks \
+    libproxy_arm64_to_x86_64_libvulkan \
+    libproxy_arm64_to_x86_64_libwebviewchromium_plat_support \
 	ndk_translation_program_runner_binfmt_misc \
-	ndk_translation.rc \
-	cpuinfo.arm64.txt \
-	ld.config.arm64.txt
+	ndk_translation.rc
 
 PRODUCT_SOONG_NAMESPACES += frameworks/libs/native_bridge_support/android_api/libc
